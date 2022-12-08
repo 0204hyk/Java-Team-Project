807517647b77af31d3c940d8d7a8725f0c730323
Gi7C-09 <Gi7C-09@Gi7C-19>
2022-12-08 14:00:04 +0900

    KioskFrame 사이즈

--- src/kiosk/KioskFrame.java
+++ src/kiosk/KioskFrame.java
@@ -1,5 +1,18 @@
 package kiosk;
 
-public class KioskFrame {
-	//
+import javax.swing.JFrame;
+
+public class KioskFrame extends JFrame {
+	
+	public KioskFrame() {
+		setLocation(500, 50);
+		setSize(650, 950);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) {
+		new KioskFrame();
+	}
 }
