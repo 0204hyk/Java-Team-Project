69d175200821cd7f0502fc91ab4c55cd85ca172a
Gi7C-09 <Gi7C-09@Gi7A-27>
2022-12-09 15:21:22 +0900

    ReceiptCheck창 완

--- src/pos/gje/ReceiptCheck/ReceiptCheckPanel.java
+++ src/pos/gje/ReceiptCheck/ReceiptCheckPanel.java
@@ -1,7 +1,9 @@
 package pos.gje.ReceiptCheck;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -9,69 +11,18 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 public class ReceiptCheckPanel extends JPanel{
-	JScrollPane scrollPane; 
-    ImageIcon icon;
-	  
-	  public ReceiptCheckPanel() {
-		  icon = new ImageIcon("src/receiptImages/날짜별 영수증 리스트 기본 틀.png");
-	       
-	        //배경 Panel 생성후 컨텐츠페인으로 지정      
-	        JPanel background = new JPanel() {
-	            public void paintComponent(Graphics g) {
-	                // Approach 1: Dispaly image at at full size
-	                g.drawImage(icon.getImage(), 0, 0, null);
-	                // Approach 2: Scale image to size of component
-	                // Dimension d = getSize();
-	                // g.drawImage(icon.getImage(), 0, 0, d.width, d.height, null);
-	                // Approach 3: Fix the image position in the scroll pane
-	                // Point p = scrollPane.getViewport().getViewPosition();
-	                // g.drawImage(icon.getImage(), p.x, p.y, null);
-	                setOpaque(false); //그림을 표시하게 설정,투명하게 조절
-	                super.paintComponent(g);
-	            }
-	        };
-	       
-	       
-	       
-	        JButton button = new JButton("버튼");
-	        background.add(button);
-	        scrollPane = new JScrollPane(background);
-	        //setContentPane(scrollPane);
-			setLayout(null);
-			setBounds(75, 80, 500, 550);		
-			setBackground(Color.white);
-	  }
-	  
-	public static JPanel listPanel() {
-		
-		
-		// 메인 Panel
-	    
-	    
-		JPanel listPanel = new JPanel();
-		
-		// 리스트 번호들
-		
-		// 영수증 리스트
-		
-		// 기본 틀
-		
-		
-		
-		
 
-		
-		
-
-		
-		// 패널의 레이아웃 설정
-		listPanel.setLayout(null);
-		listPanel.setBounds(75, 80, 500, 550);		
-		listPanel.setBackground(Color.white);
-		
-		
-		return listPanel;
-	}
+	// 패널 배경 넣기
+	public void paintComponent(Graphics g) {
+		Dimension d = getSize();
+		ImageIcon image = new ImageIcon
+				("PosImages/영수증 조회 이미지/날짜별 영수증 리스트 기본 틀.png");
+		g.drawImage(image.getImage(), 0, 0, d.width, d.height, null);
+	}	
+	
+	public ReceiptCheckPanel() {
+		setBounds(75, 80, 500, 550); // Panel 틀
+	}	       
 	
 
 	
