6c33530a7265078d92c5fc05d692b95a7b563d3d
kang <kang@192.168.0.252>
2022-12-09 14:01:24 +0900

    commit

--- src/pos/salescheck/component/chart/BarChart_AWT.java
+++ src/pos/salescheck/component/chart/BarChart_AWT.java
@@ -1,9 +1,21 @@
 package pos.salescheck.component.chart;
 
+import javax.swing.JPanel;
 
-public class BarChart_AWT  {
-	
 
-	
-	
+
+public class BarChart_AWT {
+
+
+
+	public BarChart_AWT() {
+		
+		
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+
 }
