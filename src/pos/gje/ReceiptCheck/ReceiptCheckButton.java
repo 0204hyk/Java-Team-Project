b7119f716e2d2004912b4fd1fccf80bf28df00c2
Gi7C-09 <Gi7C-09@Gi7A-27>
2022-12-09 13:59:02 +0900

    구지은 푸쉬

---
+++ src/pos/gje/ReceiptCheck/ReceiptCheckButton.java
@@ -0,0 +1,70 @@
+package pos.gje.ReceiptCheck;
+
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+public class ReceiptCheckButton {
+	
+	public ReceiptCheckButton() {
+
+	}
+	
+	public static JButton refundBtn () {
+		JButton refundBtn = new JButton();
+	
+		try {
+			BufferedImage bufferedImage = ImageIO.read(new File("src/receiptImages/환불 버튼.png"));
+			Image scaledImage = bufferedImage.getScaledInstance(230, 60, Image.SCALE_SMOOTH); // 크기 조정
+			refundBtn.setIcon(new ImageIcon(scaledImage
+					));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		refundBtn.setBounds(900, 650, 230, 60); // 위치 및 사이즈 조절
+	
+		refundBtn.setBackground(Color.white);
+		refundBtn.getFocusTraversalKeysEnabled();
+		refundBtn.setBorderPainted(false);
+		refundBtn.setOpaque(false);
+		refundBtn.setContentAreaFilled(false);
+		
+		return refundBtn;
+	}
+
+	
+	public static JButton outfutBtn() {
+		JButton outfutBtn = new JButton();
+		
+		try {
+			BufferedImage bufferedImage = ImageIO.read(new File("src/receiptImages/영수증 출력 버튼.png"));
+			Image scaledImage = bufferedImage.getScaledInstance(230, 60, Image.SCALE_SMOOTH); // 크기 조정
+			outfutBtn.setIcon(new ImageIcon(scaledImage));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		
+		outfutBtn.setBounds(650, 650, 230, 60);
+		outfutBtn.setContentAreaFilled(false);
+		outfutBtn.setBackground(Color.white);
+		outfutBtn.setBorderPainted(false);
+		outfutBtn.setOpaque(false);
+		
+		return outfutBtn;
+		
+	}
+	
+	
+
+	
+
+}
---
+++ src/pos/gje/ReceiptCheck/ReceiptCheckPanel.java
@@ -0,0 +1,47 @@
+package pos.gje.ReceiptCheck;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public class ReceiptCheckPanel {
+
+	
+	
+
+	public static JPanel listPanel() {
+		// 메인 Panel
+	    
+	    
+		JPanel listPanel = new JPanel();
+		
+		// 리스트 번호들
+		
+		// 영수증 리스트
+		
+		// 기본 틀
+		
+		
+		
+		
+
+		
+		
+
+		
+		// 패널의 레이아웃 설정
+		listPanel.setLayout(null);
+		listPanel.setBounds(75, 80, 500, 550);		
+		listPanel.setBackground(Color.white);
+		
+		
+		return listPanel;
+	}
+	
+
+	
+
+}
