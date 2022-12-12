67745b431681814271b2a10e20dcd1e28cc391b5
kang <kang@192.168.0.252>
2022-12-12 11:12:11 +0900

    commit

---
+++ src/pos/salescheck/component/SalesMain.java
@@ -0,0 +1,21 @@
+package pos.salescheck.component;
+
+import javax.swing.JFrame;
+
+public class SalesMain extends JFrame {
+
+	public SalesMain() {
+		
+		
+		
+		
+		
+		
+		setLayout(null);
+		setLocationRelativeTo(null);
+		setSize(1200, 800);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+}
