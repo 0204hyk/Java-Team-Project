4ea2135d95c2fb1ea26bc87f4b5e82768570e5dd
kang <kang@192.168.0.252>
2022-12-12 10:00:24 +0900

    버튼 클릭 시 이미지 추가.

--- src/pos/salescheck/component/button/SearchButton.java
+++ src/pos/salescheck/component/button/SearchButton.java
@@ -14,13 +14,25 @@ public class SearchButton extends JButton {
 	public SearchButton() {
 		// 매출요약 검색 버튼 이미지 설정.
 		try {
-			BufferedImage image = ImageIO.read(new File("PosImage/매출 요약 이미지/search.png"));
+			BufferedImage image = ImageIO.read(new File("PosImages/매출 요약 이미지/날짜 검색 버튼.png"));
 			Image scale = image.getScaledInstance(120, 60, Image.SCALE_SMOOTH);
 			setIcon(new ImageIcon(scale));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		// 매출요약 검색 버튼 눌렀을 때 설정.
+		try {
+			BufferedImage image = ImageIO.read(new File("PosImages/매출 요약 이미지/날짜 검색 버튼 클릭.png"));
+			Image click = image.getScaledInstance(120, 60, Image.SCALE_SMOOTH);
+			setPressedIcon(new ImageIcon(click));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
 		// 검색버튼 위치 설정.
+		
 		setBounds(750, 100, 100, 50);
 		setBorder(null);
 		
