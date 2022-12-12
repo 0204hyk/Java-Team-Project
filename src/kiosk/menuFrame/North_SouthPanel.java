d84fa823d776ae8b6a0bcdbffe25b6b2cf2cb00a
esmelshuff <114712490+esmelshuff@users.noreply.github.com>
2022-12-12 16:51:25 +0900

    메뉴 버튼 중복클릭x 구현 1.0

--- src/kiosk/menuFrame/North_SouthPanel.java
+++ src/kiosk/menuFrame/North_SouthPanel.java
@@ -3,10 +3,13 @@ package kiosk.menuFrame;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
 import javax.imageio.ImageIO;
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -25,18 +28,41 @@ public class North_SouthPanel extends JPanel {
 		GridLayout grid = new GridLayout(2, 5, 0, 5);
 		setLayout(grid);
 		int count = 0;
-		
-		for (int i = 0; i < 10; i++) {
-			JButton categoryButton = new JButton();
-			
-			try {
-				
-				count +=1;
-				
-				BufferedImage homeBufferedImage = ImageIO.read(new File
-						("KioskImages/3_메뉴선택/menuImages/"+ count + ".png"));
+		ButtonGroup group = new ButtonGroup();
 
-				categoryButton.setIcon(new ImageIcon(homeBufferedImage));
+		for (int i = 0; i < 10; i++) {
+
+			JButton categoryButton = new JButton();
+
+			group.add(categoryButton);
+
+			try {
+
+				count += 1;
+
+				BufferedImage catogoryBufferedImage = ImageIO
+						.read(new File("KioskImages/3_메뉴선택/menuImages/" + count + ".png"));
+
+				BufferedImage clcikedBufferedImage = ImageIO
+						.read(new File("KioskImages/3_메뉴선택/menuImages/" + count + "Pushed.png"));
+
+				categoryButton.setIcon(new ImageIcon(catogoryBufferedImage));
+
+				categoryButton.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+
+						//group.getSelection().setPressed(false);
+						
+						
+						categoryButton.setIcon(new ImageIcon(clcikedBufferedImage));
+					}
+				});
+
+				if (count == 6) {
+					categoryButton.doClick();
+				}
 
 			} catch (Exception e2) {
 				e2.printStackTrace();
@@ -46,8 +72,9 @@ public class North_SouthPanel extends JPanel {
 			categoryButton.setBackground(Color.white); // Opaque 사용위해서 아무색이나 지정
 			categoryButton.setOpaque(false);
 			categoryButton.setPreferredSize(new Dimension(110, 46));
+
 			add(categoryButton);
-			
+
 		}
 		
 	}
