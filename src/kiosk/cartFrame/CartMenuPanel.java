7637471414324151dd98482a54abf308319c0596
esmelshuff <114712490+esmelshuff@users.noreply.github.com>
2022-12-13 16:14:26 +0900

    장바구니 수정 (내용추가)

--- src/kiosk/cartFrame/CartMenuPanel.java
+++ src/kiosk/cartFrame/CartMenuPanel.java
@@ -1,11 +1,35 @@
 package kiosk.cartFrame;
 
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
 import javax.swing.JPanel;
 
 public class CartMenuPanel extends JPanel{
 
+	JPanel menuPanel = new JPanel();
+	
 	public CartMenuPanel() {
-		setBounds(80,240, 490, 370);
+		panelComponents();
+		setDisplay();
 	}
+	
+	public void setDisplay() {
+		setLayout(new GridLayout(1,5,0,0));
+		setBounds(80,240, 490, 370);
+		
+	}
+	
+	
+	public void panelComponents() {
+		CartMainPanelComponet compo = new CartMainPanelComponet();
+		menuPanel.setLayout(new FlowLayout(0, 10, 10));
+		
+		menuPanel.add(compo.number());
+		menuPanel.add(compo.menu());
+		menuPanel.add(compo.option());
+		add(menuPanel);
+	}
+	
 	
 }
