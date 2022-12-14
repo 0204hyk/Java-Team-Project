20183c2ebea283acd500e1ce457950cc83fbb0ff
Gi7C-09 <Gi7C-09@Gi7C-19>
2022-12-14 17:31:01 +0900

    POS 마감 파트로 좌천

--- src/pos/closing/closing_main/component/ClosingTable.java
+++ src/pos/closing/closing_main/component/ClosingTable.java
@@ -1,10 +1,14 @@
 package pos.closing.closing_main.component;
 
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import database.OjdbcConnection;
 
 public class ClosingTable extends JTable {
 	
 	public ClosingTable() {
+		String[] titleEx;
 	}
 
 }
