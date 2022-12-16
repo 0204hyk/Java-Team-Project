9221e5db77505050aa8e3006f39b0ab8cf22cfe2
구미경 <@DESKTOP-OBR64TC>
2022-12-16 13:50:58 +0900

    스크롤 적용 / JTable 진행 중

--- src/pos/gje/ReceiptCheck/receiptcheck_main/component/PrintTextArea.java
+++ src/pos/gje/ReceiptCheck/receiptcheck_main/component/PrintTextArea.java
@@ -8,11 +8,10 @@ import javax.swing.JTextArea;
 public class PrintTextArea extends JTextArea{
 
 	public PrintTextArea() {
-		//JScrollPane scrollPane = new JScrollPane(this);
-		setText("hello");
-		//contentPane.add(scrollPane);
-		setFont(new Font("맑은 고딕", Font.PLAIN, 20));	
-		setBounds(640, 95, 500, 550); // Panel 틀
-		setLayout(null);
+		
+	
+		
+		setFont(new Font("맑은 고딕", Font.PLAIN, 18));	// 폰트 기본 설정
+
 	}
 }
