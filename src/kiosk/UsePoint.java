80e1db14c78fc1b58e56c539d797d1faf28ceff4
정수정 <@192.168.219.104>
2022-12-18 19:48:19 +0900

    1

--- src/kiosk/UsePoint.java
+++ src/kiosk/UsePoint.java
@@ -39,7 +39,6 @@ public class UsePoint extends JFrame {
 		ta.setBounds(185, 370, 80, 30);
 		ta.setFont(new Font("맑은 고딕", Font.BOLD, 26));
 		ta.setForeground(Color.BLACK);
-		ta.setAlignmentX(CENTER_ALIGNMENT); // 작동안되는듯
 
 		add(wi.makeLabel("leftPointText.png", 33, 67, 335, 83));
 		add(wi.makeLabel("useAll.png", 85, 384, 63, 14));
@@ -58,8 +57,9 @@ public class UsePoint extends JFrame {
 
 		add(wi.makeLabel("pointText.png", 205, 264, 90, 28));
 		JLabel check = wi.makeLabel("check.png", 61, 366, 28, 27);
-
+		
 		check.setVisible(false);
+		
 		JButton checkBox = wi.makeButton("checkBox.png", 64, 384, 14, 14);
 		checkBox.addActionListener(new ActionListener() {
 			int num = 1;
@@ -67,13 +67,13 @@ public class UsePoint extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (num == 0) {
+					num = 1;
 					check.setVisible(false);
 					ta.setText("");
-					num = 1;
 				} else if (num == 1) {
-					check.setVisible(true);
-					ta.setText("" + cp.currentPoint());
 					num = 0;
+					check.setVisible(true);
+					ta.setText("??"+cp.currentPoint());
 				}
 
 			}
@@ -91,12 +91,12 @@ public class UsePoint extends JFrame {
 
 		add(currentPoint);
 		add(phoneNum);
-		add(ta);
 		add(use);
 		add(check);
 		add(checkBox);
 		add(wi.makeLabel("currentPointText.png", 163, 215, 121, 22));
 		add(wi.makeLabel("currentPointBox.png", 64, 188, 320, 132));
+		add(ta);
 		
 		setLayout(null);
 		setSize(461, 710); // 창 크기 이상해서 임시로 늘림
