a4008405015f8ec88b039c02b56085913d0ffcd6
Gi7C-09 <Gi7C-09@Gi7C-19>
2022-12-19 14:49:29 +0900

    main 병합

--- src/pos/salescheck/component/main/SalesMonthCheckMain.java
+++ src/pos/salescheck/component/main/SalesMonthCheckMain.java
@@ -10,18 +10,16 @@ import javax.swing.JPanel;
 import javax.swing.JTable;
 
 import pos.DigitalClock;
+import pos.ImagePanel;
 import pos.ImageScaledTool;
-import pos.closing.closing_main.container.ClosingImagePanel;
 import pos.salescheck.component.button.SalesEscapeButton;
 import pos.salescheck.component.button.SalesSearchButton;
 import pos.salescheck.component.chart.MonthChart;
-import pos.salescheck.component.combobox.DayComboBox;
 import pos.salescheck.component.combobox.MonthComboBox;
 import pos.salescheck.component.combobox.YearComboBox;
 import pos.salescheck.component.saleslist.ListImgLabel;
 import pos.salescheck.component.saleslist.TitlePanel;
 import pos.salescheck.component.saleslist.TotalLabel;
-import pos.salescheck.component.table.SalesDayTable;
 import pos.salescheck.component.table.SalesMonthTable;
 
 public class SalesMonthCheckMain extends JFrame {
@@ -32,7 +30,7 @@ public class SalesMonthCheckMain extends JFrame {
 	public SalesMonthCheckMain() {
 
 		// 매출요약 상단 메뉴바 생성.
-		JPanel title = new ClosingImagePanel(ImageScaledTool.getScaledImage(
+		JPanel title = new ImagePanel(ImageScaledTool.getScaledImage(
 				"images/PosImages/상단 메뉴바.png", 1200, 60));
 		title.setBounds(0 ,0, 1200, 60);
 
