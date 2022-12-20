b5d895ef214d370ce641986522485b7bad87807a
Gi7C-09 <Gi7C-09@Gi7A-26>
2022-12-20 12:39:13 +0900

    DayChart 수정

--- src/pos/salescheck/component/chart/DayChart.java
+++ src/pos/salescheck/component/chart/DayChart.java
@@ -64,7 +64,7 @@ public class DayChart extends JPanel {
 
 				while (rs.next()) {
 					dataset.addValue(rs.getInt("total"), 
-							rs.getString(1) + "시", rs.getString(1) + "시");
+							rs.getString(1) + "h", rs.getString(1) + "h");
 				}
 			}
 		} catch (Exception e) {
