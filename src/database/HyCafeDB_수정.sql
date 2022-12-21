3a08a7180dae7f3fe76b5a9252ec871df6b8c7ce
Gi7C-09 <Gi7C-09@Gi7C-19>
2022-12-21 17:31:03 +0900

    POS 상품 관리 쪽 에러 잡아야함,DB변경 사항에 따른 코드 수정 필요

--- src/database/HyCafeDB_수정.sql
+++ src/database/HyCafeDB_수정.sql
@@ -26,38 +26,37 @@ CREATE TABLE manager(
 
 --회원정보
 CREATE TABLE membership(
-    member_phonenumber VARCHAR2(20) 
+    member_phonenumber NUMBER(20) 
     CONSTRAINT member_phonenumber_pk PRIMARY KEY,
-    member_point NUMBER(5),
+    member_point NUMBER(6),
     member_join DATE
 );
 
--- 영수증 조회 
+-- 판매 테이블
 CREATE TABLE sales (
-   sales_number   NUMBER(20) 
-   CONSTRAINT sales_number_fk REFERENCES sales_management (sales_number),
-   member_phonenumber VARCHAR2(20) 
-   CONSTRAINT member_phonenumber_fk REFERENCES membership (member_phonenumber),
-   saledate   DATE
+    sales_number   VARCHAR2(20) 
+    CONSTRAINT sales_number_fk REFERENCES sales_management (sales_number),
+    menu_number	NUMBER(4)
+    CONSTRAINT menu_number_fk REFERENCES menu (menu_number),
+    /*
+        메뉴 옵션 : 핫/아이스 / 디카페인 / 컵종류 / 컵사이즈 / 샷 추가 / 얼음 양 / 우유  
+        해당사항 없을 경우 0 그 외에는 1,2,3 등으로 표현
+    */
+    menu_options NUMBER(7), -- 7자리 숫자로 옵션 선택 여부 설정 ex > 0122001 (선택 옵션에따른 가격 변화는 자바에서 처리)
+    menu_qty NUMBER(2),
+    price NUMBER(6)
 );
 
 --판매 관리
 CREATE TABLE sales_management (
-    sales_number   NUMBER(20)
+    sales_number   VARCHAR2(20) -- 20221221-01-163515
     CONSTRAINT sales_number_pk PRIMARY KEY,
-    menu_number	NUMBER(4)
-    CONSTRAINT menu_number_fk REFERENCES menu (menu_number),
-    option_number NUMBER(7), -- 7자리 숫자로 옵션 선택 여부 설정 ex > 0122001 (선택 옵션에따른 가격 변화는 자바에서 처리)
-    product_qty   NUMBER(3),
-    total_price NUMBER(10)
-);
-
--- 결제방식 조회 -- 얘도 sales에 합쳐도 될듯?
-CREATE TABLE payment(
-    sales_number   NUMBER(20)
-    CONSTRAINT payment_number_fk REFERENCES sales (sales_number),
-    payment_type NUMBER(1),
-    price NUMBER(10)
+    member_phonenumber NUMBER(20)
+    CONSTRAINT member_number_fk REFERENCES membership (member_phonenumber),
+    saledate   DATE,
+    payment_type NUMBER(1), -- 1 - 카드 / 2 - 포인트 / 3 - 혼합
+    used_point NUMBER(6),  -- 포인트 사용 액수
+    card_number VARCHAR2(14) -- 0000-0000-0000
 );
 
 -- date 타입 시간까지 나오게끔 포맷 변경
